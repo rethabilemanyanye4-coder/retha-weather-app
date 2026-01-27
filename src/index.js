cbe8459bb114a8db0f75902f7f4633b55332f09c
rethabilemanyanye4-coder <rethabilemanyanye4@gmail.com>
2026-01-27 18:20:41 +0200

    updated my javascript but changing the function names and then added API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+function recentWeather(response) {
+  // 1. Update the temperature
+  let temperatureElement = document.querySelector(".current-temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+
+  // 2. Update the city name to match what the API found (improves spelling)
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "f471a068ffaaf7bot45233b482c5cd2d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(recentWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  // Call the API function with the value from the search bar
+  searchCity(searchInput.value);
+}
+
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let day = date.getDay();
+  let dayIndex = date.getDay();
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   if (hours < 10) {
     hours = `0${hours}`;
   }
@@ -18,16 +43,21 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
 
-  let formattedDay = days[day];
+  let formattedDay = days[dayIndex];
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
-}
+// Event Listeners
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchSubmit);
+
+// Initial State
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+currentDateElement.innerHTML = formatDate(currentDate);
+
+// Load Paris by default when the page starts
+searchCity("Paris");
