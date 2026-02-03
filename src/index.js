a3306a97a2344202d0532f2a90242b46af63b5d5
rethabilemanyanye4-coder <rethabilemanyanye4@gmail.com>
2026-02-03 15:58:27 +0200

    adjusted the CSS and added a new function on Javascript

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function recentWeather(response) {
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
 
-  getForecast(response.data.city);
+  getforecast(response.data.city);
 
 }
 
@@ -35,6 +35,17 @@ function handleSearchSubmit(event) {
   // Call the API function with the value from the search bar
   searchCity(searchInput.value);
 }
+function formatDay(timestamp){
+  let date=new Date(timestamp +1000);
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+  return days[date.getDay()];
+
+}
+
+
+
+
 
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -62,26 +73,29 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
-function getForecast(city){
+function getforecast(city){
   let apiKey="f471a068ffaaf7bot45233b482c5cd2d";
   let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axiom(apiUrl).then(displayForecast);
+  axiom(apiUrl).then(displayforecast);
 }
-function displayForecast(response){
+function displayforecast(response){
 
   let days=['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   let forecastHtml="";
-  days.forEach(function(day){
+  response.data.daily.forEach(function(day ,index){
+    if (index < 5 ){
     forecastHtml=
+  
     forecastHtml +`
   <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon" /> 
             <div class="weather-forecast-temperature">
-              <strong>15°</strong>9°</div> 
+              <strong>${Math.round(day.temperature.maximum)}°</strong>${Math.round(day.temperature.minimum)}°</div> 
               </div>`
             
-;
+;}
 }
    );
 
@@ -100,4 +114,5 @@ currentDateElement.innerHTML = formatDate(currentDate);
 
 // Load Paris by default when the page starts
 searchCity("Paris");
-displayForecast();
+displayforecast();
+ 
